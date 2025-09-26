7be1a1b4f05dcb4e5cad68ba5e62d8ec84c6300e
IceTeaOxO <IceTeaOxO@users.noreply.github.com>
2025-09-26 13:33:52 +0000

    deploy: 22c68ff319a3d92ae38507443cf2267738907f58

---
+++ assets/AstroComponent.stories-U3Dzyho2.js
@@ -0,0 +1,5 @@
+import{c,a as d,o as u}from"./iframe-DLUB-vnc.js";import"./preload-helper-PPVm8Dsz.js";const m=(o,t)=>{const r=o.__vccOpts||o;for(const[s,n]of t)r[s]=n;return r},a={props:{},computed:{renderedComponent(){return"<div>Astro 組件的輸出</div>"}}},i={class:"astro-component-wrapper"},_=["innerHTML"];function l(o,t,r,s,n,p){return u(),c("div",i,[d("div",{innerHTML:p.renderedComponent},null,8,_)])}const f=m(a,[["render",l]]);a.__docgenInfo={exportName:"default",displayName:"AstroComponentWrapper",description:"",tags:{},sourceFiles:["/home/runner/work/story/story/src/stories/AstroComponentWrapper.vue"]};const v={title:"Astro/MyComponent",component:f,argTypes:{}},e={args:{}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    // 傳遞給組件的參數
+  }
+}`,...e.parameters?.docs?.source}}};const y=["Default"];export{e as Default,y as __namedExportsOrder,v as default};
